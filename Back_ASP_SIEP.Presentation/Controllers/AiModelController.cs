405a8d11c67897ca3f41b3aa19d802012c26d2ac
Maximilien Montier <maximilien.montier@mic-belgique.be>
2025-02-10 10:53:18 +0100

    JobSheet feature completed: controller, useCase, repository, exceptions

---
+++ Back_ASP_SIEP.Presentation/Controllers/AiModelController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Dtos.Job.Response;
+using Interfaces.UseCases.Job;
+using Exceptions.Job;
+using Dtos.Error.Response;
+
+namespace ApiControllers.AI
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AiModelController(IGetJobSheetUseCase useCase) : ControllerBase
+    {
+        private readonly IGetJobSheetUseCase _useCase = useCase;
+
+        [HttpGet("{jobName}")]
+        public async Task<IActionResult> GetJobSheet(string jobName)
+        {
+            try
+            {
+                JobSheetResponse response = await _useCase.Execute(jobName);
+                return Ok(response);
+            }
+            catch(JobSheetException ex)
+            {
+                return BadRequest(new ErrorResponse(
+                    Message: ex.Message,
+                    ErrorCode: ex.ERROR_CODE,
+                    Details: ex.InnerException?.Message
+                ));
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, new ErrorResponse(
+                    Message: "An unexpected error occurred.",
+                    ErrorCode: "001",
+                    Details: ex.Message
+                ));
+            }
+        }
+    }
+}
