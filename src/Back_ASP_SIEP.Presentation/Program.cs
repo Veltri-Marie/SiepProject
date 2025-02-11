f0220b4ad2403cbe1123b4ae71f129481579bda3
Arkanioos <elarkanioos@gmail.com>
2025-02-11 10:56:00 +0100

    Revert "Merge remote-tracking branch 'origin/dev' into knowHow"
    This reverts commit 24854d5d6c7a7061f402f1ca015bdbdd7b5d5791, reversing
    changes made to f62a17e3a6a02f2edc346caa0db98d9c0d5a6b26.

--- src/Back_ASP_SIEP.Presentation/Program.cs
+++
@@ -1,8 +0,0 @@
-using Extensions.ServiceCollection;
-
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-builder.Services.ConfigureServices();
-WebApplication app = builder.Build();
-app.MapControllers();
-app.Run();
