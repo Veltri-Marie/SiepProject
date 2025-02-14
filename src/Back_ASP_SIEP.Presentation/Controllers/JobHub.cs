b021416aa38fcde119deb33d1c1f4e06a68ce6ed
Nathan Van Quaquebeke <nathan.vanquaquebeke@mic-belgique.be>
2025-02-14 13:19:28 +0100

    SignalR add missings files

---
+++ src/Back_ASP_SIEP.Presentation/Controllers/JobHub.cs
@@ -0,0 +1,48 @@
+using Interfaces.UseCases.Job;
+using Microsoft.AspNetCore.SignalR;
+using Dtos.Job.Response;
+using Exceptions.Job;
+using Dtos.Error.Response;
+
+namespace Hubs
+{
+    public class JobHub : Hub
+    {
+        private readonly IGetJobSheetUseCase _useCase;
+
+        public JobHub(IGetJobSheetUseCase useCase)
+        {
+            _useCase = useCase;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} joined the chat");   
+        }
+
+        // public async Task GetJobSheet(string jobName)
+        // {
+        //     try
+        //     {
+        //         JobSheetResponse response = await _useCase.Execute(jobName);
+        //         await Clients.Caller.SendAsync("ReceiveJobSheet", response);
+        //     }
+        //     catch (JobSheetException ex)
+        //     {
+        //         await Clients.Caller.SendAsync("JobSheetError", new ErrorResponse(
+        //             Message: "ex.Message",
+        //             ErrorCode: ex.ERROR_CODE,
+        //             Details: ex.InnerException?.Message
+        //         ));
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         await Clients.Caller.SendAsync("JobSheetError", new ErrorResponse(
+        //             Message: "An unexpected error occurred.",
+        //             ErrorCode: "001",
+        //             Details: ex.Message
+        //         ));
+        //     }
+        // }
+    }
+}
